155c9ae31a2e87c645ecacc3a7b30f482890f3f5
BI00dSh0t <fpsbi00dsh0t@gmail.com>
2018-04-29 14:49:20 -0400

    added !purge_me command and fixed the pin recycler

---
+++ bot.py
@@ -0,0 +1,68 @@
+import os
+
+import redis
+from discord.ext import commands
+
+d = "Basic bot to keep the pins cleans... cuz dawg I'm bored"
+
+bot = commands.Bot(command_prefix='!')
+
+red = redis.from_url(os.environ.get("REDIS_URL"))
+
+
+@bot.command()
+async def recycle(num: int):
+    channel = bot.get_channel('227549008711778305')
+    pins = await bot.pins_from(channel)
+    messages = list(pins)
+
+    count = 0
+
+    for message in messages:
+        if len(message.content) > 0:
+            await bot.send_message('389590243336126466', message.content)
+        if len(message.attachments) > 0:
+            await bot.send_message('389590243336126466', message.attachments[0].get('url'))
+
+        count = count + 1
+
+        await bot.unpin_message(message)
+
+        if count >= num:
+            break
+    return
+
+
+async def purge(message):
+    if message.content.startswith('!purge'.lower()):
+        num = int(message.content.split(' ')[1])
+        await bot.purge_from(channel=message.channel, limit=num, before=message)
+    return
+
+
+async def purge_me(message):
+    if message.content.startswith('!purgeme'.lower()):
+        num = int(message.content.split(' ')[1])
+
+        counter = 0
+
+        while counter < num:
+            messages = bot.logs_from(message.channel, 100, message)
+
+            for mes in messages:
+                if mes.author == message.author:
+                    bot.delete_message(mes)
+                    counter += 1
+    return
+
+
+@bot.command()
+async def description():
+    await bot.say(d)
+    return
+
+
+bot.add_listener(purge_me, 'on_message')
+bot.add_listener(purge, 'on_message')
+
+bot.run(os.environ.get('bot_code'))
